77c9e26beac62def30835b3d8a63aad09dea216f
tomari <davy14008500@users.sourceforge.net>
2015-07-23 09:54:53 +0900

    Initial Commit

---
+++ Dhrystone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dhrystone
+//
+//  Created by davy on 2015/07/23.
+//  Copyright (c) 2015年 Arigato Java. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dhrystone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Dhrystone
+//
+//  Created by davy on 2015/07/23.
+//  Copyright (c) 2015年 Arigato Java. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
